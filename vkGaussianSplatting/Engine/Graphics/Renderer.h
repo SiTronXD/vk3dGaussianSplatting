fbe6f9b671aafb9f32c74263333bdccab06bdcd9
Simon <simonhaloman@gmail.com>
2024-03-23 19:57:01 +0100

    ADD: short comment about experimented work group size

--- vkGaussianSplatting/Engine/Graphics/Renderer.h
+++ vkGaussianSplatting/Engine/Graphics/Renderer.h
@@ -183,7 +183,7 @@ public:
 
 	const static uint32_t RS_BITS_PER_PASS = 4;
 	const static uint32_t RS_BIN_COUNT = 1u << RS_BITS_PER_PASS;
-	const static uint32_t RS_WORK_GROUP_SIZE = 64;
+	const static uint32_t RS_WORK_GROUP_SIZE = 64; // 128 saves 0.5 ms on sorting, but seems to worsen rendering timings by 1 ms (before rendering optimization).
 	const static uint32_t RS_SCAN_WORK_GROUP_SIZE = 1024;
 
 	const static uint32_t TILE_SIZE = 16;
